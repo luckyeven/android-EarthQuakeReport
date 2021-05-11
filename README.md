be591cd37d1a83a3bf8cc63a12797c5e7ae0a2fd
luckyeven <ssong094@uottawa.ca>
2021-05-10 20:27:52 -0400

    finis basic UI

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 # android-EarthQuakeReport
-This app displays a list of recent earthquakes in the world from the U.S. Geological Suvey organization.
+This app displays a list of recent earthquakes in the world from the U.S. Geological  organization.
+-----
+## Sprint1
+Building simple user interface.
--- app/src/main/java/com/example/earthquakereport/MainActivity.java
+++ app/src/main/java/com/example/earthquakereport/MainActivity.java
@@ -3,6 +3,11 @@ package com.example.earthquakereport;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +15,24 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        // An arraylist containing locations.
+        ArrayList<String> earthQuakes = new ArrayList<>();
+        earthQuakes.add("San Francisco");
+        earthQuakes.add("London");
+        earthQuakes.add("Tokyo");
+        earthQuakes.add("Mexico City");
+        earthQuakes.add("Moscow");
+        earthQuakes.add("Rio de Janeiro");
+        earthQuakes.add("Paris");
+
+        // Find a reference to the listviwe in the main layout.
+        ListView earthquakeListView = (ListView) findViewById(R.id.list);
+
+        ArrayAdapter<String> adapter = new ArrayAdapter<String>(
+                this, android.R.layout.simple_list_item_1,earthQuakes
+        );
+
+        earthquakeListView.setAdapter(adapter);
     }
 }
